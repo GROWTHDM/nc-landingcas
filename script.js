44eb4c452e86cd2384eeecb636d163a5f5b34511
GROWTHDM <135638520+GROWTHDM@users.noreply.github.com>
2023-09-28 08:29:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,15 +64,6 @@ btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
-
 
 function move() {
 
